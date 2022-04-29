ed7964a45b89f0dc5ec701032f83d4434671c088
Saeed <s.safikhani@outlook.com>
2022-04-29 16:34:25 +0200

    Create Default Window

---
+++ OpenGLCourse/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include  <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// windows dimensions
+const GLint Width = 1920, Height = 1080;
+
+int main()
+{
+    // Initialise GLFW
+    if(!glfwInit())
+    {
+        printf("GLFW Initialization failed");
+        glfwTerminate();
+        
+        return 1;
+    }
+
+    // Setup GLFW window properties
+        // Set OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core profile = No backwards compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow forward compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* mainWindow = glfwCreateWindow(Width,Height, "Test Window", NULL, NULL);
+    if(!mainWindow)
+    {
+        printf("GLFW window creation failed!");
+        glfwTerminate();
+
+        return 1;
+    }
+    
+    return 0;
+}
