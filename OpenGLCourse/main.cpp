178af274fb141e2b2ecad51340deeb2d8ed096fb
Saeed <s.safikhani@outlook.com>
2022-05-01 12:27:48 +0200

    Code setup GLEW and GLWF

--- OpenGLCourse/main.cpp
+++ OpenGLCourse/main.cpp
@@ -3,21 +3,21 @@
 #include <GLFW/glfw3.h>
 
 // windows dimensions
-const GLint Width = 1920, Height = 1080;
+const GLint Width = 1280, Height = 720;
 
 int main()
 {
     // Initialise GLFW
-    if(!glfwInit())
+    if (!glfwInit())
     {
         printf("GLFW Initialization failed");
         glfwTerminate();
-        
+
         return 1;
     }
 
     // Setup GLFW window properties
-        // Set OpenGL version
+    // Set OpenGL version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     // Core profile = No backwards compatibility
@@ -25,14 +25,50 @@ int main()
     // Allow forward compatibility
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* mainWindow = glfwCreateWindow(Width,Height, "Test Window", NULL, NULL);
-    if(!mainWindow)
+    GLFWwindow* mainWindow = glfwCreateWindow(Width, Height, "Test Window", NULL, NULL);
+    if (!mainWindow)
     {
         printf("GLFW window creation failed!");
         glfwTerminate();
 
         return 1;
     }
-    
+
+    // Get buffer size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW initialization failed");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+
+        return 1;
+    }
+
+    // Setup Viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // Get + Handle user input events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(1,0,0,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+
     return 0;
 }
