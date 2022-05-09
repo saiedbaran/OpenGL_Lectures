edf1cf03bdf2f132d7c254942692536c3d5afc22
Saeed <s.safikhani@outlook.com>
2022-05-09 14:44:19 +0200

    Simple moving triangle

--- OpenGLCourse/main.cpp
+++ OpenGLCourse/main.cpp
@@ -1,9 +1,136 @@
-﻿#include <stdio.h>
+﻿#include <complex>
+#include <cstdio>
+#include <cstring>
+
 #include  <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // windows dimensions
 const GLint Width = 1280, Height = 720;
+
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool moveRight = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triOffsetStep = 0.005f;
+
+// Vertex Shader
+static const char* vShader = R"(
+
+#version 330                                
+
+layout (location = 0) in vec3 pos;
+
+uniform float xMove;
+
+void main()
+{
+    gl_Position = vec4( 0.4 * pos.x + xMove, 0.4 * pos.y, 0.4 * pos.z, 1.0);
+}
+)";
+
+// Fragment Shader
+static const char* fShader = R"(
+
+#version 330
+
+out vec4 colour;
+
+void main()
+{
+    colour = vec4(0.0,1.0,0.0,1.0);
+}
+)";
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+    GLuint theShader = glCreateShader(shaderType);
+
+    const GLchar* theCode[1];
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(theShader, 1, theCode, codeLength);
+    glCompileShader(theShader);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+        printf("Error compiling the %d shader: '%s'\n", shaderType, eLog);
+        return;
+    }
+
+    glAttachShader(theProgram, theShader);
+}
+
+void CompileShader()
+{
+    shader = glCreateProgram();
+
+    if (!shader)
+    {
+        printf("Error creating shader program!\n");
+        return;
+    }
+
+    AddShader(shader, vShader, GL_VERTEX_SHADER);
+    AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error linking program: '%s'\n", eLog);
+        return;
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error validating program: '%s'\n", eLog);
+        return;
+    }
+
+    uniformXMove = glGetUniformLocation(shader, "xMove");
+}
+
+
+void CreateTriangle()
+{
+    GLfloat vertices[] = {
+        -1, -1, 0,
+        1, -1, 0,
+        0, 1, 0
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3,GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+}
 
 int main()
 {
@@ -56,15 +183,38 @@ int main()
     // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    CreateTriangle();
+    CompileShader();
+
     // Loop until window closed
     while (!glfwWindowShouldClose(mainWindow))
     {
         // Get + Handle user input events
         glfwPollEvents();
 
+        if (moveRight)
+        {
+            triOffset += triOffsetStep;
+        }
+        else
+        {
+            triOffset -= triOffsetStep;
+        }
+
+        if (std::abs(triOffset) >= triMaxOffset) { moveRight = !moveRight; }
+
         // Clear window
-        glClearColor(1,0,0,1);
+        glClearColor(0.5f, 0.0f, 0.1f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+
+        glUniform1f(uniformXMove, triOffset);
+        
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
     }
